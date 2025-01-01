c2c6aac2d5b4dcf4bb619d25821fe51ce25885a3
Mugees Ahmad <shmugeesahmad@gmail.com>
2025-01-01 19:37:33 +0500

    feat(linter): add eslint
    - also adds eslint to work with prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+export default withNuxt({
+	plugins: { eslintPluginPrettier },
+	rules: {
+		'eslintPluginPrettier/prettier': ['warn', { endOfLine: 'auto' }],
+		'vue/multi-word-component-names': 'off'
+	}
+});
